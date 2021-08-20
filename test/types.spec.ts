a15eefe180899afb3ea855c18cd4f1dad22223ab
louisinger <louis@vulpem.com>
2021-08-20 16:18:50 +0200

    fix types test

--- test/types.spec.ts
+++ test/types.spec.ts
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import * as assert from 'assert';
 import { describe, it } from 'mocha';
 import * as types from '../src/types';
 const typeforce = require('typeforce');
@@ -9,8 +9,8 @@ describe('types', () => {
     const buffer32byte = Buffer.alloc(32);
 
     it('return true for valid size', () => {
-      assert(types.Hash160bit(buffer20byte));
-      assert(types.Hash256bit(buffer32byte));
+      assert.default(types.Hash160bit(buffer20byte));
+      assert.default(types.Hash256bit(buffer32byte));
     });
 
     it('return true for oneOf', () => {

30c1eb72555bafeae4710b157e3c3171b60d780b
Adam Soltys <asoltys@gmail.com>
2021-09-23 22:55:34 -0700

    include dynafed block tests from rust-elements

--- test/block.spec.ts
+++ test/block.spec.ts
@@ -1,9 +1,23 @@
+import * as assert from 'assert';
 import { describe } from 'mocha';
 import * as fixtures from './fixtures/block_deserialize.json';
-import * as block from '../ts_src/block';
+import { Block } from '../ts_src/block';
 
 describe('block deserialization ', () => {
   fixtures.test.forEach(f => {
-    block.Block.fromBuffer(Buffer.from(f.hex, 'hex'));
+    it(f.name, () => {
+      let block = Block.fromBuffer(Buffer.from(f.hex, 'hex'));
+
+      if (f.name.includes('compact current')) {
+        assert.strictEqual(block.getHash().toString('hex'), f.hash);
+        assert.strictEqual(block.version, parseInt(f.version || "", 16));
+      } 
+
+      if (f.name.includes('full current')) {
+        assert.strictEqual(block.getHash().toString('hex'), f.hash);
+      } 
+
+      assert.strictEqual(block.transactions?.length, f.numOfTx);
+    });
   });
 });
